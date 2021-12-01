09336c92eb9ee1dd4c44129cfba36c97d6157aa9
Ciccio1969 <92854276+Ciccio1969@users.noreply.github.com>
2021-12-01 22:41:08 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
+//
+// Created by giova on 01/12/2021.
+//
+
+#ifndef ESERCIZIO_9_2_RIPASSO_LIB_H
+#define ESERCIZIO_9_2_RIPASSO_LIB_H
+
+bool numeriprimi (int);
+
+#endif //ESERCIZIO_9_2_RIPASSO_LIB_H
 
