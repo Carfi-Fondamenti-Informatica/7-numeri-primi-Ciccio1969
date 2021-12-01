d6716afeb1a7aadf031cc3f8aa3b80f853cbba62
Ciccio1969 <92854276+Ciccio1969@users.noreply.github.com>
2021-12-01 22:40:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
 
+using namespace std;
 int main(){
-  return 0;
+    int a=0;
+    cin >> a;
+     numeriprimi (a);
+
+    if (numeriprimi (a)== 1){
+        cout << "numero primo" << endl;
+    } else {
+        cout << " numero non primo" << endl;
+    }
+
+    return 0;
+
 }
+
+
