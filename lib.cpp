5fe8468f11eeb88e82f08ca6bb1f2e4a692711ac
Ciccio1969 <92854276+Ciccio1969@users.noreply.github.com>
2021-12-01 22:37:42 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,24 @@
+//
+// Created by giova on 01/12/2021.
+//
+
+#include "lib.h"
+bool numeriprimi ( int a){
+    bool vero_o_falso;
+
+    if (a>2){
+        for ( int b=2; b<a; b++){
+            if ((a % b)== 0){
+                vero_o_falso = false;
+            }else{
+                vero_o_falso = true;
+            }
+        }
+    }else if (a==2){
+        vero_o_falso = true;
+    }else{
+        vero_o_falso = false;
+    }
+    return vero_o_falso;
+}
 
